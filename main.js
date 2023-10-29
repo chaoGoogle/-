dc6315495ceed5503443698d23e9f93c0bb3aa07
青衫烟雨客 <3102663816@qq.com>
2023-10-29 13:30:33 +0800

    清理浏览器缓存

--- main.js
+++ main.js
@@ -1,13 +1,16 @@
-//当页面加载完成后，设置一个定时器，每隔1毫秒调用toggleSound函数
-  window.onload = function () {
-        setInterval("toggleSound()", 1);
-    }
+// 当页面加载完成后，设置一个定时器，每隔1毫秒调用toggleSound函数
+setInterval(toggleSound, 1);
 
-    function toggleSound() {
-        var music = document.getElementById("myAudio");//获取ID
-        if (music.paused) { //判读是否播放
-            music.paused = false;
-            music.play(); //没有就播放
-        }
-
+function toggleSound() {
+    const music = document.getElementById("myAudio");//获取ID
+    if (music.paused) { //判读是否播放
+        music.paused = false;
+        music.play(); //没有就播放
     }
+}
+
+// 清除浏览器的缓存
+if ('cache' in navigator) {
+    navigator.cache.clear();
+}
+
