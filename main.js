c87ed2160070430e06f039babf5042b98e15d7bc
青衫烟雨客 <3102663816@qq.com>
2023-11-01 01:02:38 +0800

    修复缓存问题

--- main.js
+++ main.js
@@ -2,6 +2,8 @@
 if ('cache' in navigator) {
     navigator.cache.clear();
 }
+// 调用清除缓存函数
+clearCache();
 
 // 定义一个清除缓存的函数
 function clearCache() {
@@ -11,15 +13,4 @@ function clearCache() {
     xhr.send();
 }
 
-// 调用清除缓存函数
-clearCache();
-//当页面加载完成后，设置一个定时器，每隔1毫秒调用toggleSound函数
-setInterval(toggleSound, 1);
 
-function toggleSound() {
-    const music = document.getElementById("myAudio");//获取ID
-    if (music.paused) { //判读是否播放
-        music.paused = false;
-        music.play(); //没有就播放
-    }
-}
