d07919fccaee81fa8397f6a235fa20e03accffd9
青衫烟雨客 <3102663816@qq.com>
2023-10-31 19:29:34 +0800

    修改浏览器缓存问题

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
 
-
 // 清除浏览器的缓存
 if ('cache' in navigator) {
     navigator.cache.clear();
 }
 
+function clearCache() {
+  const xhr = new XMLHttpRequest();
+  xhr.open("GET", window.location.href, true);
+  xhr.setRequestHeader("Cache-Control", "no-cache, no-store, must-revalidate");
+  xhr.send();
+}
+
+clearCache();
